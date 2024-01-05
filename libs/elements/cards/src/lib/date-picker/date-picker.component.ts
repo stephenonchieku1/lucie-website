b6c4b9a9300d03aa1395da7a5b7737ac0f3101c7
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-05 20:08:04 +0300

    :art:add datepicker functionality

--- libs/elements/cards/src/lib/date-picker/date-picker.component.ts
+++ libs/elements/cards/src/lib/date-picker/date-picker.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
 @Component({
   selector: 'elewa-website-date-picker',
   templateUrl: './date-picker.component.html',
   styleUrls: ['./date-picker.component.scss'],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
+  ],
+
 })
-export class DatePickerComponent {}
+export class DatePickerComponent {
+  campaignOne = new FormGroup({
+    start: new FormControl(new Date(year, month, 1)),
+    end: new FormControl(new Date(year, month, 31)),
+  });
+  campaignTwo = new FormGroup({
+    start: new FormControl(new Date(year, month, 15)),
+    end: new FormControl(new Date(year, month, 19)),
+     });
+
+
+}
