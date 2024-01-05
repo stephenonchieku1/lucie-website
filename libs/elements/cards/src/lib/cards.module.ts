251164205a977ae30d6906743358056d6e5b3ca8
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-05 16:27:51 +0300

    :sparkles:generate datepicker

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaInfoCardComponent],
-  exports: [ElewaInfoCardComponent]
+  declarations: [ElewaInfoCardComponent, DatePickerComponent],
+  exports: [ElewaInfoCardComponent],
 })
 export class CardsModule {}
