b52f85c34a7bdd4915fd0a40c103c67371146f6f
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-08 22:47:07 +0300

    :art:generate signuppage component

--- libs/features/pages/signup/src/lib/features-pages-signup.module.ts
+++ libs/features/pages/signup/src/lib/features-pages-signup.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [SignupComponent],
 })
 export class FeaturesPagesSignupModule {}
---
+++ libs/features/pages/signup/src/lib/signup/signup.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lucie-website-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent {}
