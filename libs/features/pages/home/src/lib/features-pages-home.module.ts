4444d13d22e9f2360865f4158bfec09533fda273
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-08 22:52:34 +0300

    :art:: sparkles:import login&signup to homepage

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -6,9 +6,10 @@ import { HomePageComponent } from './main/home/home-page.component';
 import { HomeRoutingModule } from './home.routing';
 
  import { ElementsLayoutFooterModule } from '@lucie-website/elements/layout/footer';
-
+ import { FeaturesPagesLoginpageModule } from '@lucie-website/features/pages/loginpage';
+import { FeaturesPagesSignupModule } from '@lucie-website/features/pages/signup';
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule,ElementsLayoutFooterModule],
+  imports: [CommonModule, HomeRoutingModule,ElementsLayoutFooterModule,FeaturesPagesLoginpageModule,FeaturesPagesSignupModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
