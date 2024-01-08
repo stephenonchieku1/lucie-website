bc0c62133ea9690ff774f9ba7d902154a41dd049
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-08 23:33:22 +0300

    add form functionality

--- libs/features/pages/loginpage/src/lib/login/login.component.ts
+++ libs/features/pages/loginpage/src/lib/login/login.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,Input,Output, EventEmitter,NgModule } from '@angular/core';
+
+
+
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'lucie-website-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
+  }
+
+
+  @Output() submitEM = new EventEmitter();
+}
